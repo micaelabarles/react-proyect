ae5ff882aa16bdf2dec1fd9b60317125681ff387
Micaela Barles <micaelabarles@gmail.com>
2021-09-07 17:29:49 -0300

    Agregando rutas de navegación

--- visca-proyect/src/App.js
+++ visca-proyect/src/App.js
@@ -1,15 +1,22 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import ItemListContainer from './components/ItemListContainer'
 import ItemDetailContainer from './components/ItemDetailContainer';
 
 function App() {
   return (
-    <div>
-     <Navbar />
-     <hr></hr>
-     <ItemListContainer />
-     <ItemDetailContainer />
-    </div>
+    <Router>
+      <Navbar />
+      <hr></hr>
+      <Switch>
+        <Route exact path='/'>
+          <ItemListContainer />
+        </Route>
+        <Route exact path='/detalle'>
+          <ItemDetailContainer />
+        </Route>
+     </Switch>
+    </Router>
   );
 }
 
